98d91443e03235b6685d1b7f81cf2937442e69b1
altertango <36963238+altertango@users.noreply.github.com>
2020-11-25 14:21:47 +0100

    Add files via upload

--- polyomino.py
+++ polyomino.py
@@ -1,7 +1,19 @@
+import os
 import itertools
 from PIL import Image
 import numpy as np
+from os.path import isfile
 
+#Function to get the names of every folder in a given path (mp)
+def onlyfolders(mp):
+    return [f for f in os.listdir(mp) if not isfile(mp+chr(92)+f)]
+
+def mk_folder(folder_name):
+    path=os.getcwd()
+    if folder_name not in onlyfolders(path):
+        os.mkdir(path+chr(92)+folder_name)
+
+#here is a function to convert points into an image
 def pt_to_im(pt):
     px=[]
     w=(255,255,255)
@@ -26,7 +38,7 @@ def pt_to_im(pt):
 
 
 
-
+#here are all possible squares to be filled
 a=[
 [1,1],
 [1,2],
@@ -41,14 +53,20 @@ a=[
 [3,3],
 [3,4]]
 
+#List of possible combinations for 4 squares
 c4=itertools.combinations(a,4)
+#for 5
 c5=itertools.combinations(a,5)
+
+#convert the points into images and save them in a folder called 4 and other called 5
 it=0
+mk_folder("4")
 for pt in c4:
     outfile=str(it)+".png"
     pt_to_im(pt).save("4\\"+outfile, "PNG")
     it+=1
 it=0
+mk_folder("5")
 for pt in c5:
     outfile=str(it)+".png"
     pt_to_im(pt).save("5\\"+outfile, "PNG")
